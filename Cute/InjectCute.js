fcebff6ee14a57b4b48406cec7195eeb6a94815c
Sarah <justsweetluna@gmail.com>
2022-12-10 13:16:06 +0100

    i eat children

--- Cute/InjectCute.js
+++ Cute/InjectCute.js
@@ -14,31 +14,3 @@ window.addEventListener('load', () => {
    Console.clear();
    Console.log('We injected bois');
 });
-
-function button() {
-   const label = document.createElement('p');
-   label.classList.add('lol-settings-window-size-text');
-   label.textContent = 'Reload Theme';
-
-   const btn = document.createElement('lol-uikit-flat-button-secondary');
-   btn.style.display = 'flex';
-   btn.textContent = 'Reload Theme';
-   btn.onclick = () => {
-      themeReload();
-   };
-
-   const row = document.createElement('div');
-   row.classList.add('lol-settings-general-row');
-   row.append(label);
-   row.append(btn);
-}
-
-function themeReload() {
-   document
-      .getElementsByTagName('body')[0]
-      .insertAdjacentHTML(
-         'afterbegin',
-         '<link rel="stylesheet" href="https://thicc-thighs.de/league-css/Cute/cute.theme.css" />'
-      );
-   location.reload();
-}
