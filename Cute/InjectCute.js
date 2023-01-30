f1907402aecea91dce1ceade3ef97e437adfc4f3
Sarah <justsweetluna@gmail.com>
2023-01-30 13:48:27 +0100

    d1 for now

--- Cute/InjectCute.js
+++ Cute/InjectCute.js
@@ -1,130 +1,98 @@
 //Name: Cute
-//Author: Sarah | ♡Sarah~#0004
-//Version: 1.0
+//Author: Sarah | â™¡Sarah~#0004
+//Version: 1.1
 //Comment: fuck rito >:c
 
-const observer = new MutationObserver((mutations) => {
-   const panel = document.querySelector(
-      'div.lol-settings-options > lol-uikit-scrollable'
-   );
-   if (
-      panel &&
-      mutations.find((record) => Array.from(record.addedNodes).includes(panel))
-   ) {
+const UI = {
+   Row: (childs) => {
       const row = document.createElement('div');
       row.classList.add('lol-settings-general-row');
-
+      if (Array.isArray(childs)) childs.forEach((el) => row.appendChild(el));
+      return row;
+   },
+   Label: (text) => {
       const label = document.createElement('p');
       label.classList.add('lol-settings-window-size-text');
-      label.textContent = 'Reload Theme';
-      label.style.marginBottom = '12px';
+      label.innerText = text;
+      return label;
+   },
+   Link: (text, href, onClick) => {
+      const link = document.createElement('p');
+      link.classList.add('lol-settings-code-of-conduct-link');
+      link.classList.add('lol-settings-window-size-text');
 
-      const searchdiv = document.createElement('searchbox-container');
-      searchdiv.style.marginBottom = '12px';
-      searchdiv.style.display = 'inline-block';
+      const a = document.createElement('a');
+      a.innerText = text;
+      a.target = '_blank';
+      a.href = href;
+      a.onclick = onClick || null;
 
-      // create a text field
-      const input = document.createElement('lol-uikit-flat-input');
-
+      link.append(a);
+      return link;
+   },
+   Button: (text, onClick) => {
+      const btn = document.createElement('lol-uikit-flat-button-secondary');
+      btn.innerText = text;
+      btn.onclick = onClick;
+      btn.style.display = 'flex';
+      return btn;
+   },
+   Input: (placeholder, onChange) => {
+      const origIn = document.createElement('lol-uikit-flat-input');
+      origIn.style.marginBottom = '12px';
       const searchbox = document.createElement('input');
       searchbox.type = 'url';
-      searchbox.placeholder = 'https://thicc-thighs.de/stuff/wallpaper.jpg';
+      searchbox.placeholder = placeholder;
       searchbox.style.width = '200px';
       searchbox.name = 'name';
-
-      const btn = document.createElement('lol-uikit-flat-button-secondary');
-      btn.style.display = 'flex';
-      btn.textContent = 'Reload theme';
-      btn.style.marginBottom = '12px';
-      btn.onclick = () => {
-         location.reload();
-         themeReload();
+      searchbox.oninput = onChange;
+      let input = {
+         get value() {
+            return searchbox.value;
+         },
       };
+      exports.search = () => input;
+      origIn.appendChild(searchbox);
+      return origIn;
+   },
+};
 
-      // if input of searchbox changes wait for 1 second
-      // and check if the value is a valid url
-      // if it is valid change the root values
-      searchbox.oninput = () => {
-         setTimeout(() => {
+// Add controls to settings panel
+const injectSettings = (panel) => {
+   panel.prepend(
+      UI.Row([
+         UI.Link(
+            'Cute-Theme',
+            'https://github.com/PrincessAkira/league-launcher-theme',
+            () => {
+               // why is this here? :wtf:
+            },
+            UI.Button('Open plugins folder', () => window.openPluginsFolder())
+         ),
+         UI.Input('https://thicc-thighs.de/stuff/wallpaper.jpg', () => {
+            let val = exports.search().value;
+
+            console.log(val + ':meow');
+            console.log(val + ':meow');
+            console.log(val + ':meow');
+            console.log(val + ':meow');
+            console.log(val + ':meow');
             if (
-               searchbox.value.match(
-                  /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|jpeg|png)/g
-               )
+               val.match(/^(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|jpeg|png)$/)
             ) {
-               accessCuteThemeCSS(searchbox.value);
-               // console.log('valid url');
-            } else {
-               //console.log('invalid url');
+               accessCuteThemeCSS(val);
             }
-         }, 1000);
-      };
+         }),
+         UI.Button('Reload theme', () => reloadTheme()),
+         document.createElement('br'),
+      ])
+   );
+};
 
-      searchdiv.append(input);
-      input.append(searchbox);
-
-      row.append(label);
-      row.append(btn);
-      row.append(input);
-
-      panel.prepend(row);
-   }
-});
-
-function downloadToAssets(filename, url) {
-   // download file
-   fetch(url)
-      .then((response) => response.blob())
-      .then((blob) => {
-         // add file to assets folder
-         const file = new File([blob], filename, { type: blob.type });
-         const reader = new FileReader();
-         reader.readAsDataURL(file);
-         reader.onloadend = function () {
-            // create new folder if it doesnt exist
-            if (!fs.existsSync('//Assets')) {
-               fs.mkdirSync('//Assets');
-               // check if cute folder exists
-               if (!fs.existsSync('//Assets//Cute')) {
-                  fs.mkdirSync('//Assets//Cute');
-               }
-            } else {
-               // check if cute folder exists
-               if (!fs.existsSync('//Assets//Cute')) {
-                  fs.mkdirSync('//Assets//Cute');
-               }
-               // check hash of file to see if it its the same
-               // if it is not the same delete the old file and add the new one
-               // if it is the same do nothing
-               if (fs.existsSync(`//Assets//Cute//${filename}`)) {
-                  const oldFile = fs.readFileSync(
-                     `//Assets//Cute//${filename}`,
-                     'utf8'
-                  );
-                  if (oldFile !== reader.result) {
-                     fs.unlinkSync(`//Assets//Cute//${filename}`);
-                     fs.writeFileSync(
-                        `//Assets//Cute//${filename}`,
-                        reader.result
-                     );
-                  }
-               } else {
-                  fs.writeFileSync(
-                     `//Assets//Cute//${filename}`,
-                     reader.result
-                  );
-               }
-               console.log('file downloaded');
-            }
-         };
-      });
-}
+///^(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)$/
 
 function accessCuteThemeCSS(value) {
    const root = document.documentElement;
-
-   // download file to assets
-   downloadToAssets('wallpaper.jpg', `url(${decodeURIComponent(value)})`);
-
    // remove formatting from the url
    // set the root values
    // decode url to get the original url
@@ -136,38 +104,60 @@ function accessCuteThemeCSS(value) {
    // if it does see if the value is the same as the new value
    // if it is not the same change the cookie value
    // if it does not exist create a new cookie
-   if (document.cookie.indexOf('background') >= 0) {
+   /* if (document.cookie.indexOf('background') >= 0) {
       if (document.cookie.indexOf(value) < 0) {
          document.cookie = `background=${value}; expires=Thu, 18 Dec 2022 12:00:00 UTC; path=/`;
       }
    } else {
       document.cookie = `background=${value}; expires=Thu, 18 Dec 2022 12:00:00 UTC; path=/`;
    }
-
+   */
    //console.log('changed background');
    //console.log(decodeURIComponent(value));
 }
 
-function themeReload() {
-   var style = document.createElement('link');
-   style.href = 'https://thicc-thighs.de/league-css/Cute/cute.theme.css';
-   style.type = 'text/css';
-   style.rel = 'stylesheet';
-   head.append(style);
+function reloadTheme() {
+   let searchRegEx = /thicc-thighs.de*/;
+   for (var i = 0; i < document.styleSheets.length; i++) {
+      if (document.styleSheets[i].href.search(searchRegEx) != -1) {
+         document.styleSheets[i].disabled = true;
+      }
+      //
+      document
+         .getElementsByTagName('body')[0]
+         .insertAdjacentHTML(
+            'afterbegin',
+            '<link rel="stylesheet" href="https://thicc-thighs.de/league-css/Cute/cute.theme.css" />'
+         );
+      location.reload();
+   }
 }
 
-window.addEventListener('load', () => {
+window.addEventListener('load', async () => {
+   // Wait for manager layer
    const interval = setInterval(() => {
       const manager = document.getElementById(
          'lol-uikit-layer-manager-wrapper'
       );
       if (manager) {
          clearInterval(interval);
-         observer.observe(manager, {
-            attributes: true,
+         // Observe settings panel
+         new MutationObserver((mutations) => {
+            const panel = document.querySelector(
+               'div.lol-settings-options > lol-uikit-scrollable'
+            );
+            if (
+               panel &&
+               mutations.some((record) =>
+                  Array.from(record.addedNodes).includes(panel)
+               )
+            ) {
+               // Inject settings
+               injectSettings(panel);
+            }
+         }).observe(manager, {
             childList: true,
             subtree: true,
-            characterData: true,
          });
       }
    }, 500);
@@ -178,5 +168,12 @@ window.addEventListener('load', () => {
          '<link rel="stylesheet" href="https://thicc-thighs.de/league-css/Cute/cute.theme.css" />'
       );
    console.clear();
+   let val = exports.search;
+   console.log(val().value + ':meow');
+   console.log(val().value + ':meow');
+   console.log(val().value + ':meow');
+   console.log(val().value + ':meow');
    console.log('We injected bois');
 });
+
+exports.UI = UI;
